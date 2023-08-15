a5dc9d38fa06778e80cef83fc780b5775e79362c
DiegoCA38 <123606613+DiegoCA38@users.noreply.github.com>
2023-08-15 16:39:56 -0600

    creacion
    Hola Mundo

---
+++ mi-archivo/src/main/java/pcsc9/mi/archivo/MiArchivo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pcsc9.mi.archivo;
+
+/**
+ *
+ * @author diego
+ */
+public class MiArchivo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
