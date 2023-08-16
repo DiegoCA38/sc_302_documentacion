06a118d98a76a4a213ac2baa36f3d099c89b6f75
JDAV1207 <90884440+JDAV1207@users.noreply.github.com>
2023-08-15 19:59:49 -0600

    Update and rename MiArchivo.java to Hola mundo_editado.java
    Paso 3, B y C

--- mi-archivo/src/main/java/pcsc9/mi/archivo/Hola mundo_editado.java
+++ mi-archivo/src/main/java/pcsc9/mi/archivo/Hola mundo_editado.java
@@ -9,8 +9,10 @@ package pcsc9.mi.archivo;
  * @author diego
  */
 public class MiArchivo {
-
     public static void main(String[] args) {
+        //Este codigo imprime el mensaje 'Hola Mundo!'
         System.out.println("Hola Mundo!");
+        
+        console.log("cambios en el archivo");
     }
 }
